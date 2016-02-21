cd46f0c9b6a6e33c175c6df028abad33427feeee
Wanghongzhe <1973214907@qq.com>
2016-02-21 15:08:59 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,7 @@
+## These functions are used to demonstrate how variables can be 
+## stored outside of the current environment for later use
+## These functions create a special object that 
+## stores a matrix and caches its inverse.
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -13,7 +17,8 @@ makeCacheMatrix <- function(x = matrix()) {
         setinverse = setinverse,
         getinverse = getinverse)
 }
-
+## Use the cached inverted matrix from function above if it exists
+## If the inverse has already been calculated use the one in cache
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     i <- x$getinverse()
